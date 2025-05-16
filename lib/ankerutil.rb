1ec971aa89a5fb65d0b02666215a1d7b760d2e25
Jesse Cao <jesse.cao@anker-in.com>
2025-05-16 11:30:20 +0800

    feat: update requir

--- lib/ankerutil.rb
+++ lib/ankerutil.rb
@@ -1,3 +1,6 @@
 module Ankerutil
+  require_relative 'ankerutil/nacos_client'
   require_relative 'ankerutil/sensitive_data'
+  require_relative 'ankerutil/string_encrypted_serialization'
+  require_relative 'ankerutil/json_encrypted_serialization'
 end
