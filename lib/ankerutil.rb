eddbdc76719c553b1244f5c3c3640e0eee432907
Jesse Cao <jesse.cao@anker-in.com>
2025-08-12 17:47:52 +0800

    feat: add require

--- lib/ankerutil.rb
+++ lib/ankerutil.rb
@@ -5,6 +5,7 @@ module AnkerUtil
   require_relative 'ankerutil/sensitive_data'
   require_relative 'ankerutil/string_encrypted_serialization'
   require_relative 'ankerutil/json_encrypted_serialization'
+  require_relative 'ankerutil/secret_manage'
 
   def self.init_sensitive_key(cbc_key, root_key, disable_write=false)
     StringEncryptedSerialization.init_sensitive_key(cbc_key, root_key, disable_write)
